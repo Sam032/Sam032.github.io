7894f8d84ae53aca5474d5de478a280268f5be9c
Samuel Lundin <samuel.lundin@email.com>
2019-08-26 15:59:11 +0200

    Initial commit

---
+++ build/precache-manifest.4469f711891846cd6f04c744341ea5f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f39d6fa72a8fae2f7f4f9781c412f7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c8daa9a1b7156295cbe",
+    "url": "/static/css/main.59efa7b9.chunk.css"
+  },
+  {
+    "revision": "852d5b110555373e5d57",
+    "url": "/static/js/2.4add4e98.chunk.js"
+  },
+  {
+    "revision": "3c8daa9a1b7156295cbe",
+    "url": "/static/js/main.6fce0ea4.chunk.js"
+  },
+  {
+    "revision": "a02f101661f3b45f33d1",
+    "url": "/static/js/runtime~main.fcc60b5f.js"
+  },
+  {
+    "revision": "7dc0a7433349f45cd1a6a57537ea88c9",
+    "url": "/static/media/bg.7dc0a743.png"
+  }
+]);
